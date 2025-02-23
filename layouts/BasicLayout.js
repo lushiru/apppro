8aaf30ac4ba7ab378e81a7d92e5d1d70b4626875
luis martinez <lushiru@hotmail.com>
2025-02-23 19:03:41 -0300

    viewloyout

--- layouts/BasicLayout.js
+++ layouts/BasicLayout.js
@@ -1,17 +1,13 @@
 import {
-  StatusBar as StatusBarRN,
-  ScrollView,
-  SafeAreaView,
+  View
 } from "react-native";
 
 export function BasicLayout(props) {
   const { children } = props;
 
   return (
-    <>
-      <StatusBarRN backgroundColor="#5ac343" />
-      <SafeAreaView />
-      <ScrollView>{children}</ScrollView>
-    </>
+    <View>
+      {children}
+    </View>
   );
 }
